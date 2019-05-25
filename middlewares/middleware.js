092d6adfdc7a6b74e64ee146ba3fd42f39c1eab6
vaideshshank <vaideshshanky@gmail.com>
2019-05-25 22:19:17 +0530

    updates

--- middlewares/middleware.js
+++ middlewares/middleware.js
@@ -7,6 +7,9 @@ module.exports={
     authenticate: (req,res,next)=>{
         var token=req.header('x-auth');
         userModel.findByToken(token).then(resp=>{
+            if(!resp){
+                res.status(400).json({message:'Authorization required. Sign in to continue.'});
+            }   
             req.user=resp;
             console.log(resp);
             req.token=token;
